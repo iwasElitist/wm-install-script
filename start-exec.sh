79d89c3b937593a4afedf06fab887f6a8496d2e4
iwasElitist <ritwiknair71@gmail.com>
2022-02-28 12:24:21 +0530

    Fixed some stuff

--- start-exec.sh
+++ start-exec.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 
 # Use the commands
-COLOR='\e[0;32m]' # Green
-echo "OPTIONS: ${COLOR}install, remove_files, update_script"
+echo "OPTIONS: \033[32minstall, remove_files, update_script\033[m"
 echo "COMMAND: "; read COMMAND
 case $COMMAND in
-    install)            $HOME/Source/wm-install-script/script-main ;;
+    install)            $HOME/Source/wm-install-script/scripts/script-main ;;
     remove_files)       rm -rf  "$HOME/Source/distro-hopping/" && rm -rf "$HOME/Source/wm-install-script/" ;;
     update_script)      git -C "$HOME/Source/distro-hopping/" pull && git -C "$HOME/Source/wm-install-script/" pull ;;
 esac
