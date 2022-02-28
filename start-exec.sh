cda941d3275beb938bc67d6d8652ee7e7fb3b4c4
iwasElitist <ritwiknair71@gmail.com>
2022-02-28 12:37:13 +0530

    Modified the start-exec.sh file

--- start-exec.sh
+++ start-exec.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # Use the commands
-echo "OPTIONS: \033[32minstall, remove_files, update_script\033[m"
-echo "COMMAND: "; read COMMAND
+printf "OPTIONS: \033[32minstall, remove_files, update_script\033[m \n"
+printf "COMMAND: "; read COMMAND; printf "\n"
 case $COMMAND in
     install)            $HOME/Source/wm-install-script/scripts/script-main ;;
     remove_files)       rm -rf  "$HOME/Source/distro-hopping/" && rm -rf "$HOME/Source/wm-install-script/" ;;
